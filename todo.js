388f323dc099c251ceac0f7214c64eeb7685b643
jfranco3 <jfranco376@gmail.com>
2022-06-16 10:55:19 -0500

    added loop

--- todo.js
+++ todo.js
@@ -29,26 +29,20 @@ const logTodos = () => {
 const populateTodos = () => {
 
     //start for loop here
-    //for ([initialExpression]; [condition]; [incrementEpression]) {
-        //action statement {
-
-    
+    for (i=0; i < arrayOfTodos.length; i++) {  
 
     let toDos = document.getElementById("todo-list")
 
     let toDoList = document.createElement("LI")
 
-    let toDoTextNode = document.createTextNode(arrayOfTodos[0].title) //first title
+    let toDoTextNode = document.createTextNode(arrayOfTodos[i].title) //first title
 
     toDoList.appendChild(toDoTextNode)  //<li>first title</li>
     
     toDos.appendChild(toDoList)
 
-    //end loop here 
-    }
-
-
-
+}//end loop here 
+} //end fxn
 
 //// Start with getting just the first item and its title property.
 //// Then capture the ol item into a variable (getElementById)
@@ -57,5 +51,9 @@ const populateTodos = () => {
 //// Now append the text to the new element
 //// Then append the element to the ol element.
 //// Put all of that inside your populateTodos function.
-// git status, add, commit, push
+//// git status, add, commit, push
 // Now that you have one element created and showing up on the screen, put the same code inside a for loop and iterate over the length of the array. But now just change out [0] for [i]! (Refer back to your for loop lesson if needed)
+
+//for (initialize for your counter; how long loop will run; increment counter) { }
+
+// for (i=0; i < arrofNums.length; i++) {  
